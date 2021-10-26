ebeeafece3126a3979b4aa92038314de262db374
xuhaoruins <xuhaoruins@hotmail.com>
2021-10-26 11:52:55 +0800

    change value

--- data.sql
+++ data.sql
@@ -7,7 +7,7 @@
 DROP TABLE IF EXISTS inventory;
 CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);
 INSERT INTO inventory (name, quantity) VALUES ('banana', 250);
-INSERT INTO inventory (name, quantity) VALUES ('orange', 154);
+INSERT INTO inventory (name, quantity) VALUES ('orange', 150);
 INSERT INTO inventory (name, quantity) VALUES ('apple', 100);
 
 -- Read
